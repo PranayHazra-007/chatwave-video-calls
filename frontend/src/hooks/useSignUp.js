dd79d92f3cf6495a1286656ba335175447f42f93
Pranay Hazra <pranayhazra034@gmail.com>
2026-06-09 12:00:11 +0530

    complete frontend setup

--- frontend/src/hooks/useSignUp.js
+++ frontend/src/hooks/useSignUp.js
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation,useQueryClient } from "@tanstack/react-query";
 import { signup } from "../lib/api";
 import {useNavigate} from "react-router-dom";
 
